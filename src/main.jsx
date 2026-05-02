c7ff2ef8524d6ffc0d4ac62b4ac58830a22fac9e
Yash Kohakade <yashkohakade@gmail.com>
2026-05-03 02:10:57 +0530

    fix: switch to RouterProvider and add layout utility classes for non-Tailwind environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import { AuthProvider } from './context/AuthContext'
 import { CollegeProvider } from './context/CollegeContext'
 import './index.css'
-import App from './App.jsx'
+import router from './router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
         <CollegeProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
+          <RouterProvider router={router} />
         </CollegeProvider>
       </AuthProvider>
     </ThemeProvider>
